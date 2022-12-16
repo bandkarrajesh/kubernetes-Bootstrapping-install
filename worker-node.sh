b84d8c6c99792c09a18d0d70fa75e04bb9e49cf3
bandkarrajesh <64117250+bandkarrajesh@users.noreply.github.com>
2022-12-16 13:01:29 +0530

    Create worker-node.sh

---
+++ worker-node.sh
@@ -0,0 +1,29 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo hostnamectl set-hostname "kube-master"
+sudo swapoff -a
+sudo sed -i 's/\/swap.img/#\/swap.img/g' /etc/fstab
+sudo tee /etc/modules-load.d/containerd.conf <<EOF
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOF
+net.bridge.bridge-nf-call-iptables = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward = 1
+EOF
+sudo sysctl --system
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update -y
+sudo apt install -y kubelet kubeadm kubectl containerd.io
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo systemctl restart containerd
+sudo systemctl enable containerd
