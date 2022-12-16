48c53a9980dec6a3fedc7a9cd754b3e6edf3bd4a
bandkarrajesh <64117250+bandkarrajesh@users.noreply.github.com>
2022-12-16 13:02:52 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -30,36 +30,3 @@ sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl restart containerd
 sudo systemctl enable containerd
 sudo kubeadm init --pod-network-cidr=192.168.10.0/24
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.24.5/manifests/tigera-operator.yaml
-sudo tee custom-resources.yaml <<EOF
-# This section includes base Calico installation configuration.
-# For more information, see: https://projectcalico.docs.tigera.io/master/reference/installation/api#operator.tigera.io/v1.Installation
-apiVersion: operator.tigera.io/v1
-kind: Installation
-metadata:
-  name: default
-spec:
-  # Configures Calico networking.
-  calicoNetwork:
-    # Note: The ipPools section cannot be modified post-install.
-    ipPools:
-    - blockSize: 26
-      cidr: 192.168.10.0/24
-      encapsulation: VXLANCrossSubnet
-      natOutgoing: Enabled
-      nodeSelector: all()
-
----
-
-# This section configures the Calico API server.
-# For more information, see: https://projectcalico.docs.tigera.io/master/reference/installation/api#operator.tigera.io/v1.APIServer
-apiVersion: operator.tigera.io/v1
-kind: APIServer
-metadata:
-  name: default
-spec: {}
-EOF
-kubectl create -f custom-resources.yaml
